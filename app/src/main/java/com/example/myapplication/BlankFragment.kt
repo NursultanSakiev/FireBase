7cbc35da593014f882087a57a8883a59880e8777
NursultanSakiev <100749627+NursultanSakiev@users.noreply.github.com>
2024-03-06 11:19:34 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/BlankFragment.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class BlankFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
